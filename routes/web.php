ea5324d7198ab15c45283d7cc29f4f984cf1c136
waieki <walid1551@hotmail.com>
2022-02-28 10:25:48 +0100

    WF oublier de cree la balise h2

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/walid', function () {
-    $saluer = 'salut';
+    $saluer = '<h1>bienvenue</h1>';
     return $saluer;
 });
